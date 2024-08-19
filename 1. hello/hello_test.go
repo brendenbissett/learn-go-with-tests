cbcca8395e5ee0facbaa836e74a690e1435ba1c7
brendenbissett <brenden.bissett@gmail.com>
2024-08-19 14:07:06 +1200

    Cleaned up tests and prepped code to contain concept of multiple languages

--- 1. hello/hello_test.go
+++ 1. hello/hello_test.go
@@ -3,9 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Brenden")
-	want := "Hello Brenden"
 
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Bob")
+		want := "Hello, Bob"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // This ensures that if the test fails, the calling function name is printed(not the helper function name)
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
